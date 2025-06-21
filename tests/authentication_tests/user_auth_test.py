e2ed5264987b02e796ae512b7b93d1c9e1f43221
arman <armanmarkov.dev@gmail.com>
2025-06-21 13:20:53 -0700

    Models more detailed

--- tests/authentication_tests/user_auth_test.py
+++ tests/authentication_tests/user_auth_test.py
@@ -11,22 +11,24 @@ def test_correct_login(auth_client, get_random_existing_registered_user):
     user = get_random_existing_registered_user
     response = auth_client.login_user_request(user=user)
 
-    validate_response_schema(LoginResponseModel, response, expected_status=HTTPStatus.OK)
+    validate_response_schema(model=LoginResponseModel, response=response, expected_status=HTTPStatus.OK)
 
 
 def test_login_with_newly_created_user(auth_client, register_new_user):
     user = register_new_user
     response = auth_client.login_user_request(user=user)
 
-    validate_response_schema(LoginResponseModel, response, expected_status=HTTPStatus.OK)
+    validate_response_schema(model=LoginResponseModel, response=response, expected_status=HTTPStatus.OK)
 
 
 @pytest.mark.parametrize("scenario_params", invalid_login_data.values(), ids=invalid_login_data.keys())
 def test_incorrect_login(make_user, auth_client, scenario_params):
     response = auth_client.login_user_request(username=scenario_params.username, password=scenario_params.password)
 
-    validate_incorrect_response(response, status=scenario_params.status_code, message=scenario_params.expected_message)
-    # Generic test for incorrect login data, does not validate response schema, since this endpoint is out of test task scope
+    validate_incorrect_response(response=response, status=scenario_params.status_code,
+                                message=scenario_params.expected_message)
+    # Generic test for incorrect login data, does not validate response schemas,
+    # since this endpoint is out of test task scope
 
 
 def test_login_with_incorrect_password(auth_client, get_random_existing_registered_user):
