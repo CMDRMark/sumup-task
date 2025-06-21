a09c6873bb0f4e84f58b2f51403bf8eb293a6956
arman <armanmarkov.dev@gmail.com>
2025-06-21 13:10:39 -0700

    Models more detailed

--- tests/authentication_tests/user_signup_test.py
+++ tests/authentication_tests/user_signup_test.py
@@ -16,17 +16,18 @@ def test_correct_signup(make_user, auth_client, save_registered_user):
     save_registered_user(user)
 
 
-@pytest.mark.skip()
-@pytest.mark.parametrize("scenario_params", invalid_signup_data.values(), ids=invalid_signup_data.keys())
-def test_incorrect_signup(make_user, auth_client, scenario_params):
-    user = make_user(username=scenario_params.username, password=scenario_params.password, autogen=False)
-    response = auth_client.register_user_request(user=user)
-
-    assert response.status_code == HTTPStatus.BAD_REQUEST, f"Response schema validation failed: {''.join(response.text)}"
-
-
 def test_signup_already_registered_user(auth_client, get_random_existing_registered_user):
     user = get_random_existing_registered_user
     response = auth_client.register_user_request(username=user.username, password=user.password)
 
     validate_incorrect_response(response, status=HTTPStatus.CONFLICT, message="Username already exists")
+
+
+@pytest.mark.skip("Skipping these tests as current implementation of the API doesnt follow simple rules for signup: "
+                  "no empty username, no empty password, etc.")
+@pytest.mark.parametrize("scenario_params", invalid_signup_data.values(), ids=invalid_signup_data.keys())
+def test_incorrect_signup(make_user, auth_client, scenario_params):
+    user = make_user(username=scenario_params.username, password=scenario_params.password, autogen=False)
+    response = auth_client.register_user_request(user=user)
+
+    validate_incorrect_response(response, status=scenario_params.status_code, message=scenario_params.expected_message)
